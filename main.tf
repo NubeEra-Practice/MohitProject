b7b084cd02ccf84b6f944d13ba68aca193006845
mujahed85 <mujahed.mca@gmail.com>
2023-08-22 21:35:43 +0530

    V3 Terraform file added

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 0.12.26"
+}
+
+provider "aws" {
+  access_key = "AKIA4DN5TV67QKAZGJ3M"
+  secret_key = "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"
+  region     = "us-east-1"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-bkt-22-aug"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+}
